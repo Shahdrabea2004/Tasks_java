065f3d6d4f1947c30463cc9f7c0d30981db64962
Shahdrabea2004 <125585806+Shahdrabea2004@users.noreply.github.com>
2024-10-08 06:21:53 +0300

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
-
 public class Task1 {
-    public static void main(String[] args) 
+    static  Scanner input = new Scanner(System.in);
+    public static void First_Code_V()
     {
-
-        Scanner input = new Scanner(System.in);
-        
-        /* 
         //First Code (V)
         int n1 = input.nextInt();
         char op = input.next().charAt(0);
@@ -17,9 +13,11 @@ public class Task1 {
             System.out.println("Right");
         else
             System.out.println("Wrong");
-        */
+    }
+
+    public static void Second_Code_K()
+    {
         
-        /* 
         //Second Code (K)
         int n = input.nextInt();
         int min=n ,max=n;
@@ -37,9 +35,11 @@ public class Task1 {
 
         }
         System.out.println(min+" "+max);
-        */
         
-        /* 
+    }
+
+    public static void Third_Code_H()
+    {
         //Third Code (H)
         int n1 = input.nextInt();
         int n2 = input.nextInt();
@@ -47,23 +47,32 @@ public class Task1 {
         System.out.println("floor "+n1+" / "+n2+" = "+(int)Math.floor(result));
         System.out.println("ceil "+n1+" / "+n2+" = "+(int)Math.ceil(result));
         System.out.println("round "+n1+" / "+n2+" = "+Math.round(result));
-        */
-        
-        /* 
+    }
+    public static void Fourth_Code_E()
+    {
         //Fourth Code (E)
         final double Pi= 3.141592653;
         double radiuas = input.nextDouble();
         System.out.println(Pi*radiuas*radiuas);
-        */
- 
-        
-        //Fiveth Code (C)
-        long n1 = input.nextLong();
-        long n2 = input.nextLong();
-        System.out.println(n1+" + "+n2+" = "+ (n1+n2));
-        System.out.println(n1+" * "+n2+" = "+ (n1*n2));
-        System.out.println(n1+" - "+n2+" = "+ (n1-n2));
-        
+    }
+    public static void Fiveth_Code_C()
+    {
+         //Fiveth Code (C)
+         long n1 = input.nextLong();
+         long n2 = input.nextLong();
+         System.out.println(n1+" + "+n2+" = "+ (n1+n2));
+         System.out.println(n1+" * "+n2+" = "+ (n1*n2));
+         System.out.println(n1+" - "+n2+" = "+ (n1-n2));
+    }
+    public static void main(String[] args) 
+    {
+
+        //First_Code_V();
+        //Second_Code_K();
+        //Third_Code_H();
+        //Fourth_Code_E();
+        //Fiveth_Code_C();
+       
 
     }
 }
